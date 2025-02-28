fc519a86a974f2b24a60fb61248e3e1e1d50d698
badinv <victor.badin@etu.univ-grenoble-alpes.fr>
2025-02-28 11:07:09 +0100

    Pour mon enzounet

--- babystep/Printer.java
+++ babystep/Printer.java
@@ -1,8 +1,17 @@
 package babystep;
-public class Printer implements IPrinter{
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Printer extends UnicastRemoteObject implements IPrinter {
+
+    protected Printer() throws RemoteException {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
     @Override
     public void print(String message) {
         System.out.println(message);
-    }    
+    }
 }
--- babystep/Server.java
+++ babystep/Server.java
@@ -1,14 +1,17 @@
 package babystep;
+
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 
 public class Server {
 
     public static void main(String[] args) {
-        IPrinter printer = new Printer();
+
         try {
+            IPrinter printer = new Printer();
             Registry registry = LocateRegistry.createRegistry(9999);
-            registry.rebind("printer", printer);
+            registry.bind("printer", printer);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
