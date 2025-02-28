7b5002e916d5344ee8628507970c78fd2fe6c18b
badinv <victor.badin@etu.univ-grenoble-alpes.fr>
2025-02-28 11:50:33 +0100

    ArrayList > tableau

--- Chat_server/ChatRoom.java
+++ Chat_server/ChatRoom.java
@@ -2,16 +2,16 @@ package Chat_server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class ChatRoom extends UnicastRemoteObject implements IChatRoom {
     private String name;
-    private IParticipant[] participants;
-    private int count;
+    private ArrayList<IParticipant> participants;
 
     public ChatRoom(String name) throws RemoteException {
         this.name = name;
-        participants = new IParticipant[10];
-        count = 0;
+        participants = new ArrayList<IParticipant>();
+
     }
 
     @Override
@@ -21,37 +21,29 @@ public class ChatRoom extends UnicastRemoteObject implements IChatRoom {
 
     @Override
     public void connect(IParticipant p) throws RemoteException {
-        participants[count++] = p;
+        participants.add(p);
+
     }
 
     @Override
-    public void leave(IParticipant p) throws RemoteException  {
-        for (int i = 0; i < count; i++) {
-            if (participants[i] == p) {
-                for (int j = i; j < count - 1; j++) {
-                    participants[j] = participants[j + 1];
-                }
-                count--;
-                break;
-            }
-        }
+    public void leave(IParticipant p) throws RemoteException {
+        participants.remove(p);
+
     }
 
     @Override
-    public String[] who() throws RemoteException  {
-        String[] names = new String[count];
-        for (int i = 0; i < count; i++) {
-            names[i] = participants[i].name();
+    public String[] who() throws RemoteException {
+        String[] names = new String[participants.size()];
+        for (int i = 0; i < participants.size(); i++) {
+            names[i] = participants.get(i).name();
         }
         return names;
     }
 
     @Override
-    public void send(IParticipant p, String msg) throws RemoteException  {
-        for (int i = 0; i < count; i++) {
-            if (participants[i] != p) {
-                participants[i].receive(p.name(), msg);
-            }
+    public void send(IParticipant p, String msg) throws RemoteException {
+        for (int i = 0; i < participants.size(); i++) {
+            participants.get(i).receive(p.name(), msg);
         }
     }
 
